ed62810b3ad661459c683c79c19bdc457516aa52
LoukasDMarkopulos <lm2059@cam.ac.uk>
2024-08-22 17:06:10 +0100

    made storing data and displaying table more efficient

--- pages/Data_Table.py
+++ pages/Data_Table.py
@@ -20,30 +20,36 @@ layout = html.Div([
     html.Div(id='output-datatable'),
 ])
 
-def parse_contents(contents, filename, date):
+
+def parse_data(contents):
+    if isinstance(contents, list):
+        contents = contents[0]
     content_type, content_string = contents.split(',')
-
     decoded = base64.b64decode(content_string)
-    try:
-        if 'csv' in filename:
-            # Assume that the user uploaded a CSV file
-            df = pd.read_csv(
+    df = pd.read_csv(
                 io.StringIO(decoded.decode('utf-8')), header = 1)
-            df.drop(df.columns[len(df.columns)-1], axis=1, inplace=True)
-        elif 'xls' in filename:
-            # Assume that the user uploaded an excel file
-            df = pd.read_excel(io.BytesIO(decoded), header = 1)
-            df.drop(df.columns[len(df.columns)-1], axis=1, inplace=True)
+    df.drop(df.columns[len(df.columns)-1], axis=1, inplace=True)
+        
+    return df
     
-    except Exception as e:
-        print(e)
-        return html.Div([
-            'There was an error processing this file.'
-            
-        ])
-    
-#    dcc.Store(id='stored-data', data=df.to_dict('records')),
 
+@callback(
+        Output('stored-data', 'data'),
+        Input('upload-data', 'contents'),
+)
+
+def store_data(contents):
+    if contents is not None:
+        df = parse_data(contents)
+        return df.to_dict('records')
+
+@callback(
+    Output('output-datatable', 'children'),
+    Input('stored-data', 'data'),
+    State('upload-data', 'filename'),
+)
+def update_table(data, filename):
+    df = pd.DataFrame.from_dict(data)
     return html.Div([
         html.H5(filename),
 
@@ -62,40 +68,3 @@ def parse_contents(contents, filename, date):
         #    'wordBreak': 'break-all'
         #})
     ])
-
-@callback(Output('output-datatable', 'children'),
-              Input('upload-data', 'contents'),
-              State('upload-data', 'filename'),
-              State('upload-data', 'last_modified'))
-def update_output(list_of_contents, list_of_names, list_of_dates):
-    if list_of_contents is not None:
-        children = [
-            parse_contents(c, n, d) for c, n, d in
-            zip(list_of_contents, list_of_names, list_of_dates)]
-        return children
-
-
-
-def parse_data(contents):
-    if isinstance(contents, list):
-        contents = contents[0]
-    content_type, content_string = contents.split(',')
-    decoded = base64.b64decode(content_string)
-    df = pd.read_csv(
-                io.StringIO(decoded.decode('utf-8')), header = 1)
-    df.drop(df.columns[len(df.columns)-1], axis=1, inplace=True)
-        
-    return df
-    
-
-@callback(
-        Output('stored-data', 'data'),
-        Input('upload-data', 'contents'),
-        #State('upload-data', 'filename'),
-        #State('upload-data', 'last_modified')
-)
-
-def store_data(contents):
-    if contents is not None:
-        df = parse_data(contents)
-        return df.to_dict('records')
